b33dec849eae0a1aa55a602e57368029be730f9b
CanYing <yian2@illinois.edu>
2022-10-02 14:34:17 -0500

    Update on 2022.10.02 for main files

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ from src_stabilizer import s1
 import os
 
 
-def main(work_dir: str, file: str):
+def main(work_dir: str, app_path: str, file: str):
     """
 
     Parameters:
@@ -14,14 +14,16 @@ def main(work_dir: str, file: str):
     """
     if not os.path.exists(work_dir):
         raise FileNotFoundError("Project Working directory not found.")
-    print("[INFO] Starting section 0:")
-    image = s0(work_dir, fname_in: str, margin: int, fname_out = None, save = True, debug = False)
-    print("[INFO] Starting section 1:")
-    image = s1(work_dir, app_path, fpath_in, fpath_out, *args)
+    print("[INFO] Starting section 0 - dense segmentation:")
+    image_crop, fname_out = s0(work_dir, fname_in=file, margin=200, fname_out=None, save=True, debug=False)
+    print("[INFO] Starting section 1 - ImageJ Stabilizer:")
+    image_sb = s1(work_dir, app_path, fname_out, fpath_out=None)  # , *args)
+    print("[INFO] Starting section 2 - CaImAn:")
+
 
 
 if __name__ == "__main__":
     try:
-        main("","")
+        main("", "")
     except:
         print()
