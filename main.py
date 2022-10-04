d5776b8528563ec4991234baaa61703f9b5fe05e
CanYing <yian2@illinois.edu>
2022-10-03 19:38:40 -0500

    Add and edit for unit test purpose

--- main.py
+++ main.py
@@ -14,16 +14,20 @@ def main(work_dir: str, app_path: str, file: str):
     """
     if not os.path.exists(work_dir):
         raise FileNotFoundError("Project Working directory not found.")
-    print("[INFO] Starting section 0 - dense segmentation:")
-    image_crop, fname_out = s0(work_dir, fname_in=file, margin=200, fname_out=None, save=True, debug=False)
-    print("[INFO] Starting section 1 - ImageJ Stabilizer:")
-    image_sb = s1(work_dir, app_path, fname_out, fpath_out=None)  # , *args)
-    print("[INFO] Starting section 2 - CaImAn:")
 
+    # print("[INFO] Starting section 0 - dense segmentation:")
+    # image_crop, fname_crop = s0(work_dir, fname_in=file, margin=200, fname_out=None, save=True, debug=False)
+
+    fname_crop = r"E:/work_dir/case1 Movie_57_crop.tif"  # for unit testing purpose
+    print("[INFO] Starting section 1 - ImageJ Stabilizer:")
+    image_sb = s1(work_dir, app_path, fname_crop)  # , *args)
+
+    # print("[INFO] Starting section 2 - CaImAn:")
+    # pass
 
 
 if __name__ == "__main__":
     try:
-        main("", "")
+        main(r"E:/work_dir", r"D:\CanYing\Fiji.app", r"E:/case1 Movie_57.tif")
     except:
-        print()
+        print("error")
