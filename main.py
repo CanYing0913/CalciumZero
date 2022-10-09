2bd751eca4260fd83e1dac614d4cc3df3158a39d
CanYing <yian2@illinois.edu>
2022-10-09 18:58:25 -0500

    Update arg parser. Missing post-handling of optional arguments.

--- main.py
+++ main.py
@@ -11,19 +11,48 @@ def parse():
     parser.add_argument('-ijp', '--imagej-path', type=str, metavar='', required=True,
                         help='Path to local Fiji ImageJ fiji folder.')
     parser.add_argument('-wd', '--work-dir', type=str, metavar='', required=True,
-                        help='Path to a folder where all intermediate and overall results are stored. If not exist then'
-                        ' will create one automatically.')
+                        help='Path to a working folder where all intermediate and overall results are stored. If not '
+                             'exist then will create one automatically.')
+    parser.add_argument('-in1', type=str, metavar='', required=True,
+                        help='Path to raw input files, supplied to auto-cropping. If provided without absolute path, '
+                             'it will assume the file exists inside working folder.')
+    parser.add_argument('-intermediate1', type=bool, default=False, required=False,
+                        help='Specified if want to begin execution starting with ImageJ stabilizer (skipping '
+                             'auto-cropping).')
+    parser.add_argument('-in2', type=str, metavar='', required=False,
+                        help='Filename to save the intermediate result 1 within working folder. If specified, '
+                             'auto-cropping will save cropped result to the filename specified, and stabilizer will '
+                             'read from it. Please only provide with relative path, it assumes to save inside working '
+                             'folder. If -intermediate1 is specified, the program will skip auto-cropping, and read '
+                             'from filename specified to start stabilizer.')
+    parser.add_argument('-intermediate2', type=bool, default=False, required=False,
+                        help='Specified if want to begin execution starting with caiman (skipping auto-cropping and '
+                             'stabilizer).')
+    parser.add_argument('-in3', type=str, metavar='', required=False,
+                        help='Filename to save the intermediate result 2 within working folder. If specified, '
+                             'stabilizer will save stabilized result to the filename specified, and caiman will read '
+                             'from it. Please only provide with relative path, it assumes to save inside working '
+                             'folder. If -intermediate2 is specified, the program will skip auto-cropping and '
+                             'stabilizer, and read from filename specified to start caiman.')
+    parser.add_argument('-out', type=str, metavar='', required=False,
+                        help='Output filename (hdf5 file) for caiman output. If not specified, it will modify caiman '
+                             'input filename to use as output filename. Please only provide with relative path, '
+                             'it assumes to save inside working folder. It will also save numpy arrays as well as '
+                             'other related debug files.')
 
+    arguments = parser.parse_args()
+    # TODO: post-process arguments
 
-    args = parser.parse_args()
-    return args
+    return arguments
 
 
 def main(work_dir: str, app_path: str, file: str):
     """
+    Main pipeline function.
 
     Parameters:
         work_dir: Directory where all temporal data are stored.
+        app_path: Path pointing to local Fiji ImageJ fiji folder.
         file: Selected raw input file path.
     """
     if not os.path.exists(work_dir):
@@ -44,7 +73,14 @@ def main(work_dir: str, app_path: str, file: str):
 
 
 if __name__ == "__main__":
-    parse()
+    # Assume parse() already takes care of handling arugments
+    args = parse()
+    ImageJ_path = args.ijp
+    work_dir = args.wd
+    fpath_in1, fpath_in2, fpath_in3 = args.in1, args.in2, args.in3
+    fapth_out = args.out
+    imm1, imm2 = args.intermediate1, args.intermediate2
+
     try:
         main(r"E:/work_dir", r"D:\CanYing\Fiji.app", r"E:/case1 Movie_57.tif")
     except:
