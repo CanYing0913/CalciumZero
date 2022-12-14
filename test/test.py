b6097989ab8306cdb9919eff5afb7f8ba5c6e50b
CanYing <yian2@illinois.edu>
2022-12-14 13:00:04 -0600

    Cleanup

---
+++ test/test.py
@@ -0,0 +1,13 @@
+import pickle
+import sys
+
+# with open(r"E:/whole_test_dir/cmn_obj", "rb") as f:
+#     cmn = pickle.load(f)
+#     print()
+if __name__ == "__main__":
+    # print(sys.argv[0])
+    year = sys.argv[1]
+    month = sys.argv[2]
+
+    print(year)
+    print(month)
