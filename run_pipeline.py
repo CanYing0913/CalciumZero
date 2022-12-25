f910898504bd6c4a198bdc9b094f8769999584da
CanYing <yian2@illinois.edu>
2022-12-24 20:12:11 -0600

    Update run_pipeline.py

--- run_pipeline.py
+++ run_pipeline.py
@@ -1,27 +1,26 @@
 import os
 import sys
+import argparse
+
+
+def parse():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-input', required=True, type=str, help='Path to your input (folder).')
+
+    return parser.parse_args()
 
 
 def main():
-    # # Run docker once to get docker container ID
-    # os.system("docker run -it my_app")
-    arg = sys.argv
-    if "-in" not in arg:
-        os.system('echo "No input specified. Program exits."')
-        os.exit(1)
-    idx = arg.index("-in")+1
-    input_path = arg[idx]
-    arg = arg[1:idx-1] + arg[idx+1:]
-    print(input_path)
-    arg = ' '.join(arg)
-    print(arg)
+    arguments = parse()
+    if not os.path.exists(arguments.input):
+        raise FileNotFoundError(f"")
+    input_path = arguments.input
+
+    container_name = ''
     input("press enter to start docker run")
-    os.system(f"docker run -v {input_path}:/tmp/CaImAn/ -t my_app:latest {arg}")
-    # # Copy inputs to container
-    # os.system(f"sudo docker cp {cid}:/tmp/input/")
-    # Pass parameter to container
-"""
-docker run --name test -v "/mnt/c/Users/canyi/Desktop/CanYing/test_docker/mnt":/tmp/mnt -i -t pipeline -input "/tmp/mnt/in/case1.tif"
-"""
+    cmd = f'docker run --name {container_name} -v "{input_path}":/tmp/mnt -i -t pipeline {arg}'
+    os.system(cmd)
+
+
 if __name__ == "__main__":
     main()
