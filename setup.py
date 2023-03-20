3393aac2d79d264c0c73b116702c33dcb580956c
CanYing0913 <74372617+CanYing0913@users.noreply.github.com>
2023-03-20 14:35:18 -0500

    Add setup.py for cx_Freeze

---
+++ setup.py
@@ -0,0 +1,19 @@
+import sys
+from cx_Freeze import setup, Executable
+
+# Dependencies are automatically detected, but it might need to be fine-tuned.
+build_exe_options = {
+    "excludes": ['PyQt5'],
+    'packages': ['imagej', 'PySimpleGUI', 'caiman']
+}
+
+# base="Win32GUI" should be used only for Windows GUI app
+base = "Win32GUI" if sys.platform == "win32" else None
+
+setup(
+    name="pipeline",
+    version="0.1",
+    description="Test on build for incomplete GUI application",
+    options={"build_exe": build_exe_options},
+    executables=[Executable("main_GUI.py", base=base)],
+)
