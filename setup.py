cc93a10c3c5d2c6eaa1374c83fb7d37fa78b8608
CanYing0913 <74372617+CanYing0913@users.noreply.github.com>
2023-03-28 17:51:21 -0500

    Add msi options and separate workflow

--- setup.py
+++ setup.py
@@ -9,13 +9,38 @@ build_exe_options = {
     'silent_level': ['1']
 }
 
+msi_data = {
+    "Icon": [
+        ("IconId", "icon.ico"),
+    ],
+}
+
+bdist_msi_options = {
+    # "add_to_path": True,
+    # "data": msi_data,
+    # "environment_variables": [
+    #     ("E_MYAPP_VAR", "=-*MYAPP_VAR", "1", "TARGETDIR")
+    # ],
+    # "upgrade_code": "{XXXXXXXX-XXXX-XXXX-XXXX-XXXXXXXXXXXX}",
+}
+
 # base="Win32GUI" should be used only for Windows GUI app
 base = "Win32GUI" if sys.platform == "win32" else None
+
+executables = [
+    Executable(
+        'main_GUI.py',
+        base=base
+    )
+]
 
 setup(
     name="pipeline",
     version="0.1",
     description="Test on build for incomplete GUI application",
-    options={"build_exe": build_exe_options},
-    executables=[Executable("main_GUI.py", base=base)],
+    options={
+        "build_exe": build_exe_options,
+        "build_msi": bdist_msi_options,
+    },
+    executables=executables,
 )
