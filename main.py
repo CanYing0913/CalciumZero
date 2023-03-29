8b40b879343720c7951910217843796d4fb44144
CanYing0913 <74372617+CanYing0913@users.noreply.github.com>
2023-03-29 11:09:54 -0500

    cleanup

--- main.py
+++ main.py
@@ -1,9 +1,5 @@
 import sys
-from time import time
-
 import src.src_pipeline as pipe
-import os
-from pathlib import Path
 
 
 def main():
@@ -11,11 +7,8 @@ def main():
     Main pipeline function.
     """
     pipeline = pipe.Pipeline()
-#     try:
     pipeline.parse()
     pipeline.run()
-#     except:
-#         print(f"{sys.exc_info()[0]} occurred.")
     # Upon exceptions, update log so user can inspect
     if pipeline.log is not None:
         print("[INFO] Closing log...")
