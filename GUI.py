54f8908e94cf5372737dcc7d25ab4e20105a6862
Yian Wang <74372617+CanYing0913@users.noreply.github.com>
2024-07-01 16:21:21 +0800

    Fix frame scroll not working bug
    Modify layout to support later ROI selection

--- GUI.py
+++ GUI.py
@@ -548,34 +548,8 @@ class GUI:
 
         qc_instance.qc_tab = qc_tab
 
-        max_h, max_w = 500, 800
-        h_, w_ = qc_instance.movies[0][0].shape
-        if h_ > max_h or w_ > max_w:
-            h_, w_ = int(h_ * min(max_h / h_, max_w / w_)), int(w_ * min(max_h / h_, max_w / w_))
-        qc_tab.canvas = tk.Canvas(qc_tab, width=w_, height=h_)
-        qc_tab.canvas.pack()
-        container = ttk.Frame(qc_tab)
-        container.pack()
-
-        container_1 = ttk.Frame(container)
-        container_1.pack()
-
-        def on_listbox_select(event):
-            # Get the index of the selected item in the Listbox
-            selected_index = qc_tab.listbox.curselection()
-            if selected_index:  # Ensure an item is selected
-                qc_tab.movie_idx = int(selected_index[0])
-                update_canvas_from_scrollbar(0)
-
-        qc_tab.listbox = tk.Listbox(container_1, selectmode=tk.SINGLE)
-        qc_tab.listbox.pack(side=tk.LEFT, fill='y')
-        for i in range(len(qc_instance.movies)):
-            qc_tab.listbox.insert(tk.END, f"Movie {i}")
-        qc_tab.listbox.config(height=qc_tab.listbox.size())
-        qc_tab.listbox.bind("<<ListboxSelect>>", on_listbox_select)
-
-        container_2 = ttk.Frame(container)
-        container_2.pack()
+        qc_tab_container = ttk.Frame(qc_tab)
+        qc_tab_container.pack()
 
         # Define a callback function to update the scrollbar and canvas
         def update_canvas_from_scrollbar(value):
@@ -588,7 +562,7 @@ class GUI:
 
             cur_frame = Image.fromarray(cur_frame)
             cur_frame = cur_frame.convert("L")  # Convert to grayscale mode
-            cur_frame = cur_frame.resize((w_, h_), Image.ANTIALIAS)
+            cur_frame = cur_frame.resize((w_, h_), Image.Resampling.LANCZOS)
             cur_frame = ImageTk.PhotoImage(cur_frame)
             qc_tab.canvas.create_image(0, 0, anchor="nw", image=cur_frame)
             qc_tab.canvas.image = cur_frame  # Keep a reference to prevent garbage collection
@@ -603,13 +577,72 @@ class GUI:
                 if 0 <= frame_idx < qc_instance.image_shape(qc_tab.movie_idx)[0]:
                     qc_tab.scrollbar.set(frame_idx)
                     update_canvas_from_scrollbar(frame_idx)
+                else:
+                    raise ValueError
             except ValueError:
                 tk.messagebox.showerror("Invalid Input", "Please enter a valid frame number.")
                 qc_tab.scrollbar.set(0)
                 qc_tab.input_field.delete(0, tk.END)
                 qc_tab.input_field.insert(0, "0")
 
-        qc_tab.scrollbar = tk.Scale(container_2, from_=0, to=qc_instance.n_images - 1, orient="horizontal",
+        def on_listbox_select(event):
+            # Get the index of the selected item in the Listbox
+            selected_index = qc_tab.listbox.curselection()
+            if selected_index:  # Ensure an item is selected
+                qc_tab.movie_idx = int(selected_index[0])
+                update_canvas_from_scrollbar(0)
+
+        qc_tab.listbox = tk.Listbox(qc_tab_container, selectmode=tk.SINGLE, height=5)
+        qc_tab.listbox.pack(side=tk.LEFT, fill='y')
+        for i in range(len(qc_instance.movies)):
+            qc_tab.listbox.insert(tk.END, f"Movie {i}")
+        qc_tab.listbox.insert(tk.END, "test")
+        qc_tab.listbox.config(height=qc_tab.listbox.size())
+        qc_tab.listbox.bind("<<ListboxSelect>>", on_listbox_select)
+
+        max_h, max_w = 500, 800
+        h_, w_ = qc_instance.image_shape(0)[1:]
+        if h_ > max_h or w_ > max_w:
+            h_, w_ = int(h_ * min(max_h / h_, max_w / w_)), int(w_ * min(max_h / h_, max_w / w_))
+        qc_tab.canvas = tk.Canvas(qc_tab_container, width=w_, height=h_)
+        qc_tab.canvas.pack(side=tk.LEFT)
+
+        # A ROI checkbox to show the ROI
+        def on_roi_en_change():
+            qc_tab.roi_enabled = roi_en.get()
+            if qc_tab.roi_enabled:
+                qc_tab.roi_scrollbar.config(state=tk.NORMAL)
+                pass
+            else:
+                qc_tab.roi_scrollbar.config(state=tk.DISABLED)
+                pass
+
+        qc_roi_container = ttk.Frame(qc_tab_container)
+        qc_roi_container.pack(side=tk.LEFT)
+        qc_tab.roi_enabled = False
+        roi_en = tk.BooleanVar(value=False)
+        qc_tab.roi_cb = tk.Checkbutton(qc_roi_container, text="Show ROI", variable=roi_en, command=on_roi_en_change)
+        qc_tab.roi_cb.pack()
+        # ROI scrollbar
+        qc_tab.roi_idx = tk.IntVar(value=0)
+        qc_tab.roi_scrollbar = tk.Scale(qc_roi_container, from_=0, to=qc_instance.n_ROIs - 1, orient="horizontal",
+                                        variable=qc_tab.roi_idx, showvalue=True)
+        qc_tab.roi_scrollbar.config(state=tk.DISABLED)
+        qc_tab.roi_scrollbar.pack(side=tk.LEFT, padx=5, pady=5)
+
+        container = ttk.Frame(qc_tab)
+        container.pack()
+
+        container_1 = ttk.Frame(container)
+        container_1.pack()
+
+        container_2 = ttk.Frame(container)
+        container_2.pack()
+
+        frame_label = ttk.Label(container_2, text="Frame Index")
+        frame_label.pack(side=tk.LEFT, padx=5, pady=5)
+        qc_tab.scrollbar = tk.Scale(container_2, from_=0, to=qc_instance.image_shape(qc_tab.movie_idx)[0] - 1,
+                                    orient="horizontal",
                                     command=update_canvas_from_scrollbar, showvalue=False)
         qc_tab.scrollbar.pack(side=tk.LEFT, padx=5, pady=5)
 
@@ -620,30 +653,13 @@ class GUI:
         # Bind the input field to the update_canvas_from_input function
         qc_tab.input_field.bind("<Return>", update_canvas_from_input)
 
-        # A ROI checkbox to show the ROI
-        def on_roi_en_change():
-            qc_tab.roi_enabled = roi_en.get()
-            if qc_tab.roi_enabled:
-                # qc_tab.roi_scrollbar.config(state=tk.NORMAL)
-                pass
-            else:
-                # qc_tab.roi_scrollbar.config(state=tk.DISABLED)
-                pass
-        qc_tab.roi_enabled = False
-        roi_en = tk.BooleanVar(value=False)
-        qc_tab.roi_cb = tk.Checkbutton(container_2, text="Show ROI", variable=roi_en)
-        qc_tab.roi_cb.pack(side=tk.LEFT, padx=5, pady=5)
-        # ROI scrollbar
-        qc_tab.roi_idx = tk.IntVar(value=0)
-        qc_tab.roi_scrollbar = tk.Scale(container_2, from_=0, to=qc_instance.n_ROIs - 1, orient="horizontal",
-                                        variable=qc_tab.roi_idx, showvalue=True)
-        qc_tab.roi_scrollbar.config(state=tk.DISABLED)
-        qc_tab.roi_scrollbar.pack(side=tk.LEFT, padx=5, pady=5)
-
         close_button = ttk.Button(qc_tab, text='Close',
                                   command=lambda id=instance_tab.id: self.close_instance(id))
         close_button.pack(side='top', padx=5, pady=5)
         qc_tab.close_button = close_button
+
+        # Initializations go here
+        update_canvas_from_scrollbar(0)
 
         instance_tab.notebook.add(qc_tab, text='QC')
         instance_tab.notebook.qc_tab = qc_tab
